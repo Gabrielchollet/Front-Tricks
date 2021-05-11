4c22a526ecac029eb7a2f792499c8f1ba1960e2f
Gabriel Chollet <62822641+Gabrielchollet@users.noreply.github.com>
2021-05-10 23:55:58 -0300

    Transformar notas escolares
    Crie um algoritmo que transforme as notas do sistema
    numérico para sistema de notas em caracteres tipo A B C

---
+++ discover js/README.md
@@ -0,0 +1,9 @@
+##Conteudo:
+
+Exercícios aplicados no guia estelar de Javascript da maratona Discover.
+
+##Exercícios:
+- [] Sistema de notas escolares
+- [] Fluxo de caixa familiar
+- [] Celsius to Fahrenheit
+- [] Buscando e encontrando dados em Array
---
+++ discover js/exercicio_01.js
@@ -0,0 +1,17 @@
+/* ### Transformar notas escolares
+
+Crie um algoritmo que transforme as notas do sistema
+numérico para sistema de notas em caracteres tipo A B C
+
+* de 90 para cima - A
+* de 80 - 89      - B
+* de 70 - 79      - C
+* de 60 - 69      - D
+* de 50 - 59      - F
+
+*/
+
+/* guarda os valores das notas em um array */
+
+
+
