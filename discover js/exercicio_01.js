70b5e304de74f7cf6466ba61595caaf05d93e4c0
Gabriel Chollet <62822641+Gabrielchollet@users.noreply.github.com>
2021-05-13 00:01:28 -0300

    Update exercicio_01.js

--- discover js/exercicio_01.js
+++ discover js/exercicio_01.js
@@ -11,7 +11,31 @@ numérico para sistema de notas em caracteres tipo A B C
 
 */
 
-/* guarda os valores das notas em um array */
+var nota = prompt("Digite sua nota: ");
 
+convert(nota);
 
-
+function convert(value) {
+    if (value > 90) 
+    {
+        value = 'A'
+        alert("Sua nota é " + value)
+    }
+    else if (value > 80) {
+        value = 'B'
+        alert("Sua nota é " + value)
+    }
+    else if (value > 70) {
+        value = 'C'
+        alert("Sua nota é " + value)
+    }
+    else if (value > 60) {
+        value = 'D'
+        alert("Sua nota é " + value)
+    }
+    else (value > 50)
+    {
+        value = 'F'
+        alert("Sua nota é " + value)
+    }
+}
